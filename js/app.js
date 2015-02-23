c1ded52d43c3a744784567245f3d9be3d365b983
Savagebanana <adriandornellas@gmail.com>
2015-02-23 01:17:10 -0500

    stylesheet add
    added the site stylesheet

---
+++ js/app.js
@@ -0,0 +1,112 @@
+// Foundation JavaScript
+// Documentation can be found at: http://foundation.zurb.com/docs
+$(document).foundation({
+	orbit: {
+		animation: 'fade', // Sets the type of animation used for transitioning between slides, can also be 'fade'
+		timer_speed: 7000, // Sets the amount of time in milliseconds before transitioning a slide
+		pause_on_hover: true, // Pauses on the current slide while hovering
+		resume_on_mouseout: true, // If pause on hover is set to true, this setting resumes playback after mousing out of slide
+		animation_speed: 500, // Sets the amount of time in milliseconds the transition between slides will last
+		slide_number: false,
+		navigation_arrows: false,
+		swipe: true,
+		bullets: false
+	}});
+
+	// grab an element
+	var myElement = document.getElementById("header");
+	// construct an instance of Headroom, passing the element
+	var headroom  = new Headroom(myElement);
+	// initialise
+	headroom.init(); 
+
+
+
+
+// Open Coach Profiles
+
+$('.project-item').click(function () {
+
+	var projectID = '#' + $(this).data('project-id');
+	
+	$('.project').hide();
+	
+	$(projectID).show();
+
+	$('html, body').animate({
+		scrollTop: $(projectID).offset().top - 76
+	}, 600);
+
+});
+// close coach profiles
+
+$('.project-close').click(function () {
+
+	$(this).closest('.project').hide();
+
+	$('html, body').animate({
+		scrollTop: $('#our-team').offset().top - 76
+	}, 600);
+
+});
+
+// //  Contact form
+
+$( "#contact" ).submit(function( event ) {
+	console.log('contact script submit');
+	$(this).each(function(){
+		var count = $(this).find(':input[data-invalid]').length;
+		if (count === 0) {
+			
+			var url = "./php/form.php";
+			var data = $(this).serialize();
+			var success = $(this).append( "<p>Thank You! Your message has been sent.</p>" );
+
+		$.ajax({
+			type: "POST",
+			url: url,
+			data: data,
+			success: success
+		});
+
+		}
+	});
+
+	event.preventDefault();
+
+});
+
+function submitForm(){
+	console.log("TestSubmit");
+	//$(this).each(function(){
+		//var count = $(this).find(':input[data-invalid]').length;
+		//if (count === 0) {
+	var url = "./php/form.php";
+	var data  = {};
+	$("#week").find(":input").each(function() {
+	// The selector will match buttons; if you want to filter
+	// them out, check `this.tagName` and `this.type`; see
+	// below	
+	if($(this).attr("type")==="checkbox"){
+		data[this.name] = $(this).prop('checked');
+	}
+	else{
+		data[this.name] = $(this).val();
+	}
+	});
+	var success = $("#sucmessage").append( "<p>Thank You! Your message has been sent. You can reach Coach Paul at (416) 707-8850 with any questions, and he will be in contact with you this week to confirm your registration and collect your payment.</p>" );
+
+			$.ajax({
+				type: "POST",
+				url: url,
+				data: data,
+				success: success
+			});
+}
+
+// // Add camper function
+function addCamper(){
+	document.getElementById('kid-two').style.display = 'block';
+	document.getElementById('add-camper').style.display = 'none';
+}
+
---
+++ php/form.php
@@ -0,0 +1,101 @@
+<?php
+require_once("PHPMailer-master/PHPMailerAutoload.php");
+
+/* Cleaning Data */
+
+foreach ($_POST as $key => $value) {
+	$_POST[$key] = trim($_POST[$key]);
+	$_POST[$key] = stripslashes($_POST[$key]);
+	$_POST[$key] = htmlspecialchars($_POST[$key]);
+}
+
+
+/* Variables */
+$mail = new PHPMailer;
+
+$mail->isSMTP();
+$mail->SMTPAuth = true;
+$mail->SMTPSecure = 'ssl';
+$mail->Host = 'host262.hostmonster.com';
+$mail->Port = 465;
+$mail->Username = 'automated@360futbolcamp.com';
+$mail->Password = 'FormPass2015'; //old gmail pass: Z3RV7nRTcp
+$mail->isHTML(true);
+$mail->addAddress('info@360futbolcamp.com');
+
+if($_POST['hidden']== "contact"){
+
+	$mail->From = $_POST['email'];
+	$mail->FromName = $_POST['name'];
+	$mail->addReplyTo($_POST['email']);
+	$mail->Subject = $_POST['subject'];
+	$mail->Body = $_POST['message'];
+
+} else if($_POST['hidden'] == "register"){
+
+	$mail->Subject = "Registration";
+	$mail->Name = $_POST['parent-name'];
+	$mail->From = $_POST['parent-email'];
+	$mail->FromName = $_POST['parent-name'];
+	$mail->addReplyTo($_POST['parent-email']);
+	$mail->Body .= "Parent Name: ".$_POST['parent-name']."<br>";
+	$mail->Body .= "Email: ".$_POST['email']."<br>";
+	$mail->Body .= "Phone #: ".$_POST['phone']."<br>";
+	$mail->Body .= "Address: ".$_POST['address']."<br>";
+	$mail->Body .= "City: ".$_POST['city']."<br>";
+	$mail->Body .= "Postal Code: ".$_POST['postal-code']."<br>";
+	$mail->Body .= "Emergency Contact: ".$_POST['emergency-contact']."<br>";
+	$mail->Body .= "Emergency Phone Number: ".$_POST['emergency-phone']."<br>";
+	$mail->Body .= "Relation to camper/s: ".$_POST['relation']."<br>";
+	$mail->Body .= "Camper 1 Name: ".$_POST['camper-one-name']."<br>";
+	$mail->Body .= "Birthday: ".$_POST['birth-month']." ".$_POST['birth-day']." ".$_POST['birth-year']."<br>";
+	$mail->Body .= "Shirt size C1: ".$_POST['shirt-size']."<br>";
+	$mail->Body .= "Allergies: ".$_POST['allergies']."<br>";
+	$mail->Body .= "Meds Required: ".$_POST['meds-req']."<br>";
+	if ($_POST['camper-2-name']!=""){
+
+		$mail->Body .= "Camper 2 Name: ".$_POST['camper-one-name']."<br>";
+		$mail->Body .= "Birthday: ".$_POST['birth-month-2']." ".$_POST['birth-day-2']." ".$_POST['birth-year-2']."<br>";
+		$mail->Body .= "Shirt size C1 ".$_POST['shirt-size-2']."<br>";
+		$mail->Body .= "Allergies: ".$_POST['allergies-2']."<br>";
+		$mail->Body .= "Meds Required: ".$_POST['meds-req-2']."<br>";
+
+	}
+	$mail->Body .= "Weeks Selected: <br>";
+	if($_POST['A1']=="true"){
+		$mail->Body .= "&nbsp;&nbsp;&nbsp;July 20-24<br>";
+	}
+	if($_POST['A2']=="true"){
+		$mail->Body .= "&nbsp;&nbsp;&nbsp;July 27-31<br>";
+	}
+	if($_POST['B1']=="true"){
+		$mail->Body .= "&nbsp;&nbsp;&nbsp;August 3-7<br>";
+	}
+	if($_POST['B2']=="true"){
+		$mail->Body .= "&nbsp;&nbsp;&nbsp;August 10-14<br>";
+	}
+	if($_POST['C1']=="true"){
+		$mail->Body .= "&nbsp;&nbsp;&nbsp;August 17-21<br>";
+	}
+	if($_POST['C2']=="true"){
+		$mail->Body .= "&nbsp;&nbsp;&nbsp;August 24-28<br>";
+	}
+
+	$mail->Body .="<br>";
+
+
+
+
+} else{
+	$mail->From = 'adrian@savagebanana.com';
+	$mail->FromName = 'Adrian';
+	$mail->addReplyTo('adrian@savagebanana.com');
+	$mail->Subject = 'Failed';
+	$mail->Body = 'Not working';
+}
+
+if(!$mail->send()){
+	die();
+}
+
+?>
